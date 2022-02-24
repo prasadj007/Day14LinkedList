d20e9211635be075a55bfff005c9fe01850cf05f
Prasad <prasadj007official@gmail.com>
2022-02-25 00:38:04 +0530

    Added LinkedList UC2 to add at start

---
+++ UC2_AddAtStart/Runner.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.uc2;
+
+public class Runner<T> {
+
+	public static void main(String[] args) {
+		LinkedList <Integer> obj = new LinkedList<Integer>();
+		obj.add(70);
+		obj.addAtStart(30);
+		obj.addAtStart(56);
+		obj.show();
+	}
+}

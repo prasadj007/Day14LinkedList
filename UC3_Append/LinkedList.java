f3333441b88eb473e87929dc080eb88827495254
Prasad <prasadj007official@gmail.com>
2022-02-25 00:42:41 +0530

    Added LinkedList UC3 with Append

---
+++ UC3_Append/LinkedList.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.uc3;
+
+	public class LinkedList<T> {
+
+		MyNode<T> head;
+
+		public void add(T data) {
+			MyNode<T> newNode = new MyNode<>();
+			newNode.data = data;
+			if (head == null) {
+				head = newNode;
+
+			} else {
+				MyNode<T> node = head;
+				while (node.next != null) {
+					node = node.next;
+				}
+				node.next = newNode;
+			}
+		}
+
+
+		public  void addAtStart (T data) {
+			MyNode<T> node =new MyNode<>();
+			node.data=data;
+			node.next=head;
+			head=node;
+		}
+
+		public void show() {
+			MyNode<T> node = head;
+			while (node.next != null) {
+				System.out.println(node.data);
+				node=node.next;;
+			}
+			System.out.print(node.data);
+		}
+
+	}
+
---
+++ UC3_Append/MyNode.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.uc3;
+
+public class MyNode <T> {
+	T data;
+	MyNode<T>next;
+}
